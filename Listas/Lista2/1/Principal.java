5b4653fd7e0c3e6fba08238266fe0d642b725858
joaopedronardari <joaonardari@terra.com.br>
2014-06-07 15:52:09 -0300

    Primeiro exercicio lista2

---
+++ Listas/Lista2/1/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println("exercicio1");
+		Par<String,Integer> par = new Par<String,Integer>("String",1234567);
+
+		System.out.println(par.primeiro());
+		System.out.println(par.segundo());
+		System.out.println(par.maior());
+	}
+}
